c03ea76d390efb1b74ccaede764cc70ee5d5dcde
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2023-10-05 13:22:03 +0530

    ui patches

--- app/routes.py
+++ app/routes.py
@@ -28,18 +28,24 @@ def scrape():
         # Scrape the Wikipedia page
         result = get_internal_urls(url)
 
+        final_result = []
         # Save responses to the database
         for internal_url in result:
             content_lines = scrape_url_content(internal_url, limit=3)
             content_lines_str = '\n'.join(content_lines)
+            single_data = {
+                'url' : internal_url,
+                'content_lines' : content_lines
+            }
+            final_result.append(single_data)
             print(f"content lines {internal_url}: {content_lines}")
             response = ResponseModel(url=internal_url, content=content_lines_str)  # Initialize with empty content
             db.session.add(response)
             db.session.commit()
 
         # Render the index.html template with the scraped results
-        responses = ResponseModel.query.all()
-        print(f"responses.................... : {responses}")
-        return render_template('index.html', result=responses)
+        latest_response = ResponseModel.query.order_by(ResponseModel.id.desc()).first()
+        print(f"responses.................... : {final_result}")
+        return render_template('index.html', result=final_result)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
